733833ca9ebf004ce2276d4a19933bb80d8210f8
betomukii <mukiibeatrice4@gmail.com>
2024-01-18 19:28:01 +0300

    made changes

--- monty.h
+++ monty.h
@@ -15,7 +15,7 @@
 
 
 /**
- * struct stack_t - doubly linked list representation of a stack (or queue)
+ * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
@@ -23,11 +23,11 @@
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO
  */
-typedef struct stack_t
+typedef struct stack_s
 {
 	int n;
-	struct stack_t *prev;
-	struct stacks_t *next;
+	struct stack_s *prev;
+	struct stacks_s *next;
 } stack_t;
 
 /**
